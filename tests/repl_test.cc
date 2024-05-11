4b5dbe957999fee708bb7065f28e295cd9eba4eb
CodeItQuick <evanontario@hotmail.com>
2024-05-11 08:36:19 -0300

    add repl tests

---
+++ tests/repl_test.cc
@@ -0,0 +1,10 @@
+//
+// Created by Owner on 5/11/2024.
+//
+#include <gtest/gtest.h>
+#include "repl_test.h"
+
+TEST(ReplTests, CanReceiveInput) {
+
+    EXPECT_TRUE(true);
+}
---
+++ tests/repl_test.h
@@ -0,0 +1,14 @@
+//
+// Created by Owner on 5/11/2024.
+//
+
+#ifndef CTDDSQLLITE_REPL_TEST_H
+#define CTDDSQLLITE_REPL_TEST_H
+
+
+class repl_test {
+
+};
+
+
+#endif //CTDDSQLLITE_REPL_TEST_H
